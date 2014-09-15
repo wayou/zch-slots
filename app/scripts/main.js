babb6898a47c6c4c93f315f1bc6f1a6be91bcc9b
wayou <liuwayong@gmail.com>
2014-09-15 21:45:46 +0800

    normalize

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+var Slots = (function() {
+    //the main slot class
+    return {
+        init: function() {
+            //initialize all stuff
+            console.info('game initalized!');
+        }
+    };
+});
+
+Slots.init();
