28d58834b514e25066c7a49ca999fe9a8d0948cf
Wayou Liu <liuwayong@gmail.com>
2014-09-16 21:28:26 +0800

    refactor the size

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,34 @@
+/**
+ *中彩汇slots machine
+ *
+ *note:
+ *machine body image sie:606*563
+ *image padding left 25px
+ *
+ **/
+
+//the main slot class
+
 var Slots = (function() {
-    //the main slot class
-    
+    var WINDOW_WIDTH = window.innerWidth,
+        WINDOW_HEIGHT = window.innerHeight,
+        bodyImagesSize = {
+            width: 606,
+            height: 563
+        },
+        realBodyImageWidth = ~~WINDOW_WIDTH,
+        realBodyImageHeight = ~~ ((bodyImagesSize.height / bodyImagesSize.width) * realBodyImageWidth),
+        canvas = $('#canvas')[0],
+        ctx = canvas.getContext("2d");
+
+
+
     return {
         init: function() {
             //initialize all stuff
-            console.info('game initalized!');
+            //set canvas size
+            ctx.canvas.width = realBodyImageWidth,
+            ctx.canvas.height = realBodyImageHeight;
         }
     };
 })();
