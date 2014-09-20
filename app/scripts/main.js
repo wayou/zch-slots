428bd0cee6ebba740957ab90be4e35ac1303c41b
wayou <liuwayong@gmail.com>
2014-09-20 16:29:36 +0800

    more logic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,11 @@
  *note:
  *machine body image sie:640*377
  *image padding left 25px
- *
+ *wheel start (85,90) from left top
+ *wheel width 90px
+ *wheel gutter 5px
+ *wheel height 240px
+ *wheelCanvasWidth 540-85=455
  **/
 
 //the main slot class
@@ -19,35 +23,130 @@ var Slots = function() {
         width: 96,
         height: 96
     };
+
     this.realBodyImageWidth = ~~this.WINDOW_WIDTH;
     this.ratio = this.realBodyImageWidth / this.bodyImagesSize.width; //the ratio of the image on the screen relative to the orignal image size
-    this.realBodyImageHeight = ~~ (this.ratio * this.bodyImagesSize.height);
+
     this.canvas = $('#canvas')[0];
-    this.ctx = canvas.getContext("2d");
+    this.ctx = this.canvas.getContext('2d');
+    this.realBodyImageHeight = ~~ (this.ratio * this.bodyImagesSize.height);
+    this.canvas = null;
+    this.ctx = null;
     this.$spinBtn = $('#spinBtn');
-    this.GAME_STATUS = 0; //0 stop, 1 running
+    this.GAME_STATUS = 0; // 0 stopped, 1 running, 2 result loaded
+    this.score = 0;
+    this.ITEM_CNT = 10; //how many icons in this game
+    this.user = {
+        uid: 0,
+        user_name: 0,
+        wealth: 0, //得分
+        user_info_ready: false
+    };
+    this.items = {
+        icons: [], //store all icons into this array
+        readyCnt: 0 //how many icons are loaded
+    };
+    this.game = {
+        bet: 1000, //1000 per line
+        lineCnt: 1
+    };
+    this.wheel = null;
 };
 
 Slots.prototype = {
     //initialize all stuff
     init: function() {
         var that = this;
-        //set canvas size
-        this.ctx.canvas.width = this.realBodyImageWidth;
-        this.ctx.canvas.height = this.realBodyImageHeight;
+
+        // initilaixe the wheel
+        this.wheel = {
+            top: ~~(90 * that.ratio),
+            left: ~~(85 * that.ratio),
+            width: ~~(90 * that.ratio),
+            height: ~~(240 * that.ratio),
+            gutter: ~~(5 * that.ratio),
+            canvasWidth: ~~(455 * that.ratio),
+            canvasHeight: ~~(240 * that.ratio)
+        }
         //position the spin button
         this.$spinBtn.find('img').css('width', ~~ (this.ratio * this.spinImagesSize.width));
 
+        //initialize the canvas
+        this.initCanvas(that);
+
+        //load icons
+        this.loadResource(that);
+
+        //load user data
+        this.getUserData(that);
+
         // listen the spin button 
-        $('#spinBtn').on('tap click', function() {
-            if (that.GAME_STATUS == 1) {
-                return;
+        this.$spinBtn.on('tap click', function() {
+            if (that.checkValidation) {
+                //here we go 
+                console.info('game start!');
+                that.spin(that);
             }
-            alert('game started!');
         });
     },
-    spin: function() {
-        //
+    initCanvas: function(entry) {
+        $("#canvas").attr({
+            width: entry.wheel.canvasWidth,
+            height: entry.wheel.canvasHeight
+        }).css({
+            top: entry.wheel.top,
+            left: entry.wheel.left
+        });
+
+    },
+    checkValidation: function(entry) {
+        if (entry.GAME_STATUS == 0 /*the game is ready to start*/ && entry.user.user_info_ready /*user data is ready so we know if the user has enough wealth to start the game*/ && entry.user.wealth >= (entry.game.bet * entry.game.lineCnt) /*the wealth is enough to start*/ && entry.items.readyCnt == entry.ITEM_CNT /*all 15 icons are loaded*/ ) {
+            //here we go 
+            return true;
+        } else {
+            return false;
+        }
+    },
+    loadResource: function(entry) {
+        for (var i = entry.ITEM_CNT; i > 0; i--) {
+            entry.items.icons[i] = new Image();
+            entry.items.icons[i].src = 'images/items/' + i + '.png';
+            entry.items.icons[i].onload = function() {
+                entry.items.readyCnt++;
+            };
+        }
+    },
+    getUserData: function(entry) {
+        //get user info from server
+        // $.getJSON('/userinfo', function(data) {
+        //     //set the user info
+        //     // entry.user.wealth = data.wealth;
+        //     // entry.user.user_info_ready = true;
+        // });
+
+        //lock
+        entry.user.wealth = 10000;
+        entry.user.user_info_ready = true;
+    },
+    spin: function(entry) {
+        entry.GAME_STATUS = 1;
+
+        //send the bet info the server and start the animation while waiting the result from the server
+        // $.post('/bet', {
+        //     uid: 001,
+        //     bet:1000,
+        //     lineCnt:1,
+        //     totalBet:entry.game.bet * entry.game.lineCnt
+        // }, function(result) {
+        //     //here we get the lines and combos result, and draw them out
+        // })
+
+
+        //start the animation
+
+    },
+    drawIcons: function(canvas, layout /*an array specify the layout of the icons*/ ) {
+
     }
 };
 
