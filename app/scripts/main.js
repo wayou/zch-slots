4e1074ebe1522240d28f281cc0c6f3057e28f429
wayou <liuwayong@gmail.com>
2014-09-20 14:16:50 +0800

    build the fundamental

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,35 +2,47 @@
  *中彩汇slots machine
  *
  *note:
- *machine body image sie:606*563
+ *machine body image sie:640*377
  *image padding left 25px
  *
  **/
 
 //the main slot class
-var Slots = (function() {
-    var WINDOW_WIDTH = window.innerWidth,
-        WINDOW_HEIGHT = window.innerHeight,
-        bodyImagesSize = {
-            width: 606,
-            height: 563
-        },
-        realBodyImageWidth = ~~WINDOW_WIDTH,
-        realBodyImageHeight = ~~ ((bodyImagesSize.height / bodyImagesSize.width) * realBodyImageWidth),
-        canvas = $('#canvas')[0],
-        ctx = canvas.getContext("2d");
-
-
-
-    return {
-        init: function() {
-            //initialize all stuff
-            //set canvas size
-            ctx.canvas.width = realBodyImageWidth,
-            ctx.canvas.height = realBodyImageHeight;
-        },
-        spin:function()
+var Slots = function() {
+    this.WINDOW_WIDTH = window.innerWidth;
+    this.WINDOW_HEIGHT = window.innerHeight;
+    this.bodyImagesSize = {
+        width: 640,
+        height: 377
     };
-})();
+    this.spinImagesSize = {
+        width: 96,
+        height: 96
+    };
+    this.realBodyImageWidth = ~~this.WINDOW_WIDTH;
+    this.ratio = this.realBodyImageWidth / this.bodyImagesSize.width; //the ratio of the image on the screen relative to the orignal image size
+    this.realBodyImageHeight = ~~ (this.ratio * this.bodyImagesSize.height);
+    this.canvas = $('#canvas')[0];
+    this.ctx = canvas.getContext("2d");
+    this.$spinBtn = $('#spinBtn');
+};
 
-Slots.init();
+Slots.prototype = {
+    init: function() {
+        //initialize all stuff
+        //set canvas size
+        this.ctx.canvas.width = this.realBodyImageWidth;
+        this.ctx.canvas.height = this.realBodyImageHeight;
+        //position the spin button
+        this.$spinBtn.find('img').css('width', ~~ (this.ratio * this.spinImagesSize.width));
+    },
+    spin: function() {
+        //
+    }
+};
+
+//invoke our game
+$(function() {
+    var zchSlots = new Slots();
+    zchSlots.init();
+});
