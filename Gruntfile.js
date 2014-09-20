b290e1957e32b3b8bd621cd2330d7d053b341ca8
Wayou Liu <liuwayong@gmail.com>
2014-09-20 21:50:15 +0800

    disable rename in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -172,11 +172,11 @@ module.exports = function (grunt) {
             dist: {
                 files: {
                     src: [
-                        '<%= config.dist %>/scripts/{,*/}*.js',
-                        '<%= config.dist %>/styles/{,*/}*.css',
-                        '<%= config.dist %>/images/{,*/}*.*',
-                        '<%= config.dist %>/styles/fonts/{,*/}*.*',
-                        '<%= config.dist %>/*.{ico,png}'
+                        // '<%= config.dist %>/scripts/{,*/}*.js',
+                        // '<%= config.dist %>/styles/{,*/}*.css',
+                        // '<%= config.dist %>/images/{,*/}*.*',
+                        // '<%= config.dist %>/styles/fonts/{,*/}*.*',
+                        // '<%= config.dist %>/*.{ico,png}'
                     ]
                 }
             }
